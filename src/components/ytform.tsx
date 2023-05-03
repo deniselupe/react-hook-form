dae44772033341d2aae7a35f1ca6923de89c7931
deniselupe <dgrodriguez24@hotmail.com>
2023-05-02 21:56:22 -0500

    rhf: display error messages in UI

--- src/components/ytform.tsx
+++ src/components/ytform.tsx
@@ -13,15 +13,17 @@ this issue discussion.
 */
 
 /*
-So far in this code I have:
-    - Installed react-hook-form and @hookform/devtools
-    - Used react-hook-form's useForm() hook
-    - Learned about the different properties that are included in the object useForm() returns
-    - Learned of the different properties that are included in the object register() returns
-    - Using register() properties to track a form control
-    - Using <DevTools /> to get a visual demonstration of the properties being tracked for a form control
-    - Adding a second argument to register() to add validatio such as required, regex, min & max, etc
-    - Creating a onSubmit handler function and passing it into useForms().handleSubmit() function to get access to the form's data
+To summarize the changes in this code:
+    - We have to make the error messages 
+      we specified for each form control show in the UI.
+
+To display error mesages:
+    - destructure the `formState` object from useForm()
+    - and then destructure the `errors` object from the `formState` object
+    - the `errors` object contains errors for every field that has failed validations
+    - we can access the `message` property for a field, and display it in the UI
+
+This is how you support HTMl validations with react-hook-form.
 */
 const DevT: React.ElementType = dynamic(
   () => import('@hookform/devtools').then((module) => module.DevTool),
@@ -38,8 +40,9 @@ interface FormValues {
 const YouTubeForm = () => {
     // Let useForm know that <FieldValues> Type Parameter will equal to <FormValues>
     const form = useForm<FormValues>();
-    const { register, control, handleSubmit } = form;
+    const { register, control, handleSubmit, formState } = form;
     const { name, ref, onChange, onBlur } = register("username", { required: 'Username is required.' }); // Value is <input />'s name attr value 
+    const { errors } = formState;
 
     // passing onSubmit as an argument to handleSubmit gives it access to the form data, which we
     // can log to the console
@@ -51,20 +54,27 @@ const YouTubeForm = () => {
         <div>
             <h1>YouTube Form</h1>
             <form onSubmit={handleSubmit(onSubmit)} noValidate>
-                <label htmlFor="username">Username</label>
-                <input type="text" id="username" name={name} ref={ref} onChange={onChange} onBlur={onBlur} />
-                <br />
-                <label htmlFor="email">E-mail</label>
-                <input type="email" id="email" {...register("email", { 
-                    pattern: {
-                        value: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
-                        message: 'Invalid email format'
-                    } 
-                })} />
-                <br />
-                <label htmlFor="channel">Channel</label>
-                <input type="text" id="channel" {...register("channel", { required: { value: true, message: 'Channel is required' } })} />
-                <br />
+                <div className="form-control">
+                    <label htmlFor="username">Username</label>
+                    <input type="text" id="username" name={name} ref={ref} onChange={onChange} onBlur={onBlur} />
+                    <p className="error">{errors.username?.message}</p>
+                </div>
+                <div className="form-control">
+                    <label htmlFor="email">E-mail</label>
+                    <input type="email" id="email" {...register("email", { 
+                        pattern: {
+                            value: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+                            message: 'Invalid email format'
+                        } 
+                    })} />
+                    <p className="error">{errors.email?.message}</p>
+                </div>
+                <div className="form-control">
+                    <label htmlFor="channel">Channel</label>
+                    <input type="text" id="channel" {...register("channel", { required: { value: true, message: 'Channel is required' } })} />
+                    <p className="error">{errors.channel?.message}</p>
+                </div>
+
                 <button>Submit</button>
             </form>
             <DevT control={control} />
